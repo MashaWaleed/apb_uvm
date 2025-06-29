cc8311ca0494e3aeef4749e62dd7630f4dd57083
Abdelrahman M.Ali <abdelrahmansalby23@gmail.com>
2025-06-29 03:54:08 +0300

    minior update

--- interfaces/APB_interface.sv
+++ interfaces/APB_interface.sv
@@ -12,22 +12,8 @@ interface APB_interface(
     logic [DATA_WIDTH-1:0] PWDATA;
     logic [PSTRB_WIDTH-1:0] PSTRB;
 
-    //inputs from Periphral to Slave 
-    logic ready;
-    logic [DATA_WIDTH-1:0] readData;
-
     //outputs from Slave to Master
     logic [DATA_WIDTH-1:0] PRDATA;
     logic PSLVERR;
-
     logic PREADY;
-
-
-    //outputs from Slave to Periphral
-    logic [DATA_WIDTH-1:0]writeData;
-    logic address;
-
-    logic write_read;
-    logic enable;
-    logic strb;
 endinterface
